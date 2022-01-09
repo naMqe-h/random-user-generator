997a68518dfc2afeefe1e0266a222a347a1ccd6d
naMqe <naMqe07@gmail.com>
2022-01-10 00:04:04 +0100

    refactoring getPesel function

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const {male, female, femaleLastName, maleLastName} = require('./data')
 
 const app = express()
@@ -39,7 +39,49 @@ app.get("/api/user-data", (req, res) => {
     }
 
     const getPesel = () => {
-        pesel = Math.floor(Math.random() * 89999999999 + 10000000000)
+        let tempPesel = []
+        let lastDigits = year % 100
+
+        // digits: [*][*][][][][][][][][][]
+        if(lastDigits < 10) tempPesel.push('0' + lastDigits)
+        else tempPesel.push(lastDigits.toString())
+
+        // digits: [][][*][*][][][][][][][] 
+        if(year >= 1800 && year <= 1899) tempPesel.push((parseInt(dateSplit[1]) + 80).toString())
+        if(year >= 1900 && year <= 1999) tempPesel.push(dateSplit[1])
+        if(year >= 2000 && year <= 2099) tempPesel.push((parseInt(dateSplit[1]) + 20).toString())
+
+        // digits: [][][][][*][*][][][][][]
+        if(parseInt(dateSplit[0]) < 10) tempPesel.push('0' + dateSplit[0])
+        else tempPesel.push(dateSplit[0])
+
+        // digits: [][][][][][][*][*][*][*][]
+        let fourDigits
+        if( gender == 'male') fourDigits = (Math.floor(Math.random() * 4999) * 2) + 1
+        else fourDigits = Math.floor(Math.random() * 4999) * 2
+
+        if(fourDigits < 10) fourDigits = '000' + fourDigits
+        else if(fourDigits < 100) fourDigits = '00' + fourDigits
+        else if(fourDigits < 1000) fourDigits = '0' + fourDigits
+        
+        tempPesel.push(fourDigits.toString())
+
+        // digits: [][][][][][][][][][][*]
+        let controlSum = (
+                9 * tempPesel[0][0] + 
+                7 * tempPesel[0][1] +  
+                3 * tempPesel[1][0] + 
+                1 * tempPesel[1][1] + 
+                9 * tempPesel[2][0] + 
+                7 * tempPesel[2][1] + 
+                3 * tempPesel[3][0] + 
+                1 * tempPesel[3][1] + 
+                9 * tempPesel[3][2] + 
+                7 * tempPesel[3][3]
+            ) % 10
+        tempPesel.push(controlSum)
+        
+        pesel = tempPesel.join('')
     }
 
     if(count && parseInt(count) > 3000) res.send({error: 'Maksymalna ilość użytkowników na jednym zapytaniu to 3000'})
@@ -69,3 +111,4 @@ app.get("/api/user-data", (req, res) => {
 });
 
 app.listen(5000)
+// 03 21 17 0629 3
